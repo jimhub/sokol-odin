9ebf71abf8954f0eb4b7c42f6980c6510ac12bb9
James Liljenquist <jliljenq@gmail.com>
2024-09-27 15:16:43 -0600

    Squashed commit of the following:
    commit 65214ee71f322dd7523af8eec2b1e684d81b2438
    Merge: cde0ed1 11dcdf0
    Author: jimhub <jliljenq@gmail.com>
    Date:   Thu Aug 29 17:32:16 2024 -0600
    
        Merge branch 'floooh:main' into main
    
    commit cde0ed19adba34f8b8232638407b466443915f90
    Merge: 3e5ee9a 70ab7de
    Author: jimhub <jliljenq@gmail.com>
    Date:   Thu Aug 22 21:52:42 2024 -0600
    
        Merge branch 'floooh:main' into main
    
    commit 3e5ee9a05b93e5796ace6fd03108303ab7aa69fb
    Merge: f91d408 d6a44f1
    Author: jimhub <jliljenq@gmail.com>
    Date:   Wed Jul 31 12:49:27 2024 -0600
    
        Merge pull request #2 from jimhub/sokol-gp
    
        Sokol gp
    
    commit d6a44f1a7385d830112ca7f984f947d59a34f0e4
    Merge: 9a87075 f91d408
    Author: jimhub <jliljenq@gmail.com>
    Date:   Wed Jul 31 12:44:16 2024 -0600
    
        Merge pull request #1 from floooh/main
    
        Bring it in
    
    commit 9a87075c036e63990d2c6ae4aac2aa4b70edd6e6
    Author: James Liljenquist <jliljenq@gmail.com>
    Date:   Thu Jul 11 21:31:57 2024 -0600
    
        HATE
    
    commit 6453be39fd84cdd5563e05800baadfa1e8261587
    Author: James Liljenquist <jliljenq@gmail.com>
    Date:   Thu Jul 11 20:54:25 2024 -0600
    
        Added sokol-gp

--- sokol/c/sokol_gfx.c
+++ sokol/c/sokol_gfx.c
@@ -1,5 +1,9 @@
 #if defined(IMPL)
 #define SOKOL_GFX_IMPL
+#define SOKOL_GP_IMPL
 #endif
 #include "sokol_defines.h"
 #include "sokol_gfx.h"
+
+/* sokol_gp depends directly on the _implementation_ of sokol_gfx, so it's being lumped in with the gfx lib */
+#include "sokol_gp.h"
